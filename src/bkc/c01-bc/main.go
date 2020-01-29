e951a286aea14e1c1f0707d8bb55d462639a07fd
YIMING SHI <shiyiming@outlook.com>
2020-01-28 22:10:33 -0800

    70.实现钱包数据持久化
    实例钱包模块集成
    1.将钱包创建功能加入命令行操作
    2.实现获取地址列表功能
    3.钱包功能持久化

--- src/bkc/c01-bc/main.go
+++ src/bkc/c01-bc/main.go
@@ -1,22 +1,13 @@
 package main
 
+import "bkc/c01-bc/BLC"
+
 func main(){
 
-	//bc:=BLC.CreateBlockCHainWithGenesisBlock()
-	//defer bc.DB.Close()
-	////上链
-	//bc.AddBlock([]byte("ron sent 10 tc to aaron"))
-	//bc.AddBlock([]byte("jacky sent 10 tc to aaron"))
-	//
-	//bc.PrintChain()
 
 	cli:=BLC.CLI{}
 	cli.Run()
 
-	//result:=BLC.Base58Encode([]byte("this is the example"))
-	//fmt.Printf("result:%s\n",result)
-	//
-	//decodeResult:=BLC.Base58Decode([]byte("NK2smnfSzALMcNJ8YHsxUJMrfN"))
-	//fmt.Printf("decode result:%s\n",decodeResult)
+
 
 }
